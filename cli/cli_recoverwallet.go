fc0eb2269122c1c3ee7b42057f6cfb39434b7fbb
zebra <15828809311@163.com>
2019-04-27 16:54:13 +0800

    修复了当首次启动时，使用recoverwallet出现钱包文件不存在的bug

--- cli/cli_recoverwallet.go
+++ cli/cli_recoverwallet.go
@@ -3,15 +3,11 @@ package cli
 import (
 	"blockchain_go/wallet"
 	"fmt"
-	"log"
 )
 
 func (cli *CLI) recoverWallet(code []string, nodeID string) {
-	wallets, err := wallet.NewWallets(nodeID)
-	if err != nil {
-		log.Panic(err)
-	}
-	// TODO 当第一次启动节点时，钱包文件不存在
+	wallets, _ := wallet.NewWallets(nodeID)
+
 	address := wallets.RecoverWallet(code)
 	fmt.Printf("Your address: %s\n", address)
 	wallets.SaveToFile(nodeID)
