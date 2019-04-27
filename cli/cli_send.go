573e5d084de0769067fac0aa514a3a7a6c9d1e17
zebra <15828809311@163.com>
2019-04-27 16:45:06 +0800

    修改了命令行中send，当from为wallets时，从wallets中找wallet发起交易

--- cli/cli_send.go
+++ cli/cli_send.go
@@ -5,16 +5,16 @@ import (
 	"blockchain_go/miner"
 	"blockchain_go/net"
 	"blockchain_go/transaction"
-	w "blockchain_go/wallet"
+	ws "blockchain_go/wallet"
 	"fmt"
 	"log"
 )
 
 func (cli *CLI) send(from, to string, amount int, nodeID string, mineNow bool) {
-	if !w.ValidateAddress(from) {
+	if (!ws.ValidateAddress(from)) && (from != "wallets") {
 		log.Panic("ERROR: Sender address is not valid")
 	}
-	if !w.ValidateAddress(to) {
+	if !ws.ValidateAddress(to) {
 		log.Panic("ERROR: Recipient address is not valid")
 	}
 
@@ -22,22 +22,32 @@ func (cli *CLI) send(from, to string, amount int, nodeID string, mineNow bool) {
 	UTXOSet := blockchain.UTXOSet{bc}
 	defer bc.Close()
 
-	wallets, err := w.NewWallets(nodeID)
+	wallets, err := ws.NewWallets(nodeID)
 	if err != nil {
 		log.Panic(err)
 	}
-	wallet := wallets.GetWallet(from)
 
-	tx := w.NewUTXOTransaction(&wallet, to, amount, &UTXOSet)
+	txs := make([]*transaction.Transaction, 0)
+	if from != "wallets" {
+		wallet := wallets.GetWallet(from)
+		txs = append(txs, ws.NewUTXOTransaction(&wallet, to, amount, &UTXOSet))
+	} else {
+		txs = append(txs, ws.NewUTXOTransactionByWallets(wallets, to, amount, &UTXOSet)...)
+	}
+	//tx := w.NewUTXOTransaction(&wallet, to, amount, &UTXOSet)
 
 	if mineNow {
 		cbTx := transaction.NewCoinbaseTX(from, "")
-		txs := []*transaction.Transaction{cbTx, tx}
+		//txs := []*transaction.Transaction{cbTx, txs[0]}
+		txs = append([]*transaction.Transaction{cbTx}, txs...)
 
 		newBlock := miner.MineBlock(bc, txs)
 		UTXOSet.Update(newBlock)
 	} else {
-		net.SendTx(net.CenterNode, tx)
+		for _, tx := range txs {
+			//_ = net.SendTx(net.CenterNode, tx)
+			net.BroadcastTx(tx)
+		}
 	}
 
 	fmt.Println("Success!")
